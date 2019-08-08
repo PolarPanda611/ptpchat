b089f39c6b9f162f445f495559d4d62699017e50
daniel <daniel.gj.tan@gmail.com>
2019-08-08 14:15:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Point to Point Chat Example
+
+example extends from github.com/gorilla/websocket/examples/chat
+
+go run main.go
+
+hub  # goroutine to store all the connection
---
+++ hub.go
@@ -0,0 +1,64 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"strings"
+)
+
+// Hub maintains the set of active clients and broadcasts messages to the
+// clients.
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		// new conn
+		case client := <-h.register:
+			h.clients[client] = true
+		// lost conn
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		//receive message
+		case message := <-h.broadcast:
+			messagestring := string(message[:])
+			tousername := strings.Split(messagestring, ";")[1]
+			for client := range h.clients {
+				if client.Name == tousername {
+					select {
+					case client.send <- message:
+					default:
+						close(client.send)
+						delete(h.clients, client)
+					}
+				}
+			}
+		}
+	}
+}
